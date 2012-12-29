ee9bfdc944e096f9cdb40c3ab38d653dfea3ca1e
Programus <programus@gmail.com>
2012-12-29 23:04:03 +0800

    Changed image scale and compress method. To enable video recording, the preview size must be the max one, so change preview size is not possible. The new method is scaling raw NV21 data before converting and transfer.

--- projects/Lookie-Camera/src/org/programus/nxt/android/lookie_camera/MainActivity.java
+++ projects/Lookie-Camera/src/org/programus/nxt/android/lookie_camera/MainActivity.java
@@ -20,6 +20,8 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
@@ -111,7 +113,7 @@ public class MainActivity extends Activity {
 					p.startCameraPreview();
 					break;
 				case Constants.SIZE:
-					p.selectPreviewSize(cmd.getFormat());
+//					p.selectPreviewSize(cmd.getFormat());
 					break;
 				case Constants.LIGHT:
 					if (cmd.getFormat() > 0) {
@@ -207,16 +209,6 @@ public class MainActivity extends Activity {
 		public void onPreviewFrame(byte[] data, Camera camera) {
 			long time = System.currentTimeMillis();
 			Log.d(TAG, "preview frame:" + time);
-			CameraCommand cmd = new CameraCommand();
-			Camera.Parameters params = camera.getParameters();
-			Camera.Size size = params.getPreviewSize();
-			cmd.setCommand(Constants.CAMERA);
-			cmd.setFormat(params.getPreviewFormat());
-			cmd.setWidth(size.width);
-			cmd.setHeight(size.height);
-			cmd.setImageData(compressImage(data, camera));
-			cmd.setSystemTime(time);
-			
 			// the time past from last sent
 			long dt = time - prevSentTime;
 			// the max possible transfer data size during this period
@@ -224,6 +216,17 @@ public class MainActivity extends Activity {
 //			logger.log(String.format("dt: %d, max: %d, size: %d", dt, maxDataLimit, this.prevDataLength));
 			Log.d(TAG, String.format("dt: %d, max: %d, size: %d", dt, maxDataLimit, this.prevDataLength));
 			if (this.prevDataLength < maxDataLimit || this.prevDataLength == 0) {
+				CameraCommand cmd = new CameraCommand();
+				Camera.Parameters params = camera.getParameters();
+				Camera.Size size = params.getPreviewSize();
+				int width = 256;
+				int height = width * size.height / size.width;
+				cmd.setCommand(Constants.CAMERA);
+				cmd.setFormat(params.getPreviewFormat());
+				cmd.setWidth(width);
+				cmd.setHeight(height);
+				cmd.setImageData(getImageDataForSend(data, camera, width, height));
+				cmd.setSystemTime(time);
 				// last cargo is smaller than the limit
 				synchronized(sendQ) {
 					Log.d(TAG, String.format("time: %d, len: %d", cmd.getSystemTime(), cmd.getImageData().length));
@@ -238,17 +241,90 @@ public class MainActivity extends Activity {
 		}
 	};
 	
-	private byte[] compressImage(byte[] data, Camera camera) {
+	private byte[] getImageDataForSend(byte[] data, Camera camera, int width, int height) {
 		Camera.Parameters params = camera.getParameters();
 		Camera.Size size = params.getPreviewSize();
-		int format = params.getPreviewFormat();
-		YuvImage yuvImage = new YuvImage(data, format, size.width, size.height, null);
-		Rect rect = new Rect(0, 0, size.width, size.height);
+		byte[] scaled = this.scaleNV21Image(data, size.width, size.height, width, height);
+		byte[] jpg = this.compressYuvImage2Jpeg(scaled, params.getPreviewFormat(), Constants.COMPRESS_RATE, width, height);
+//		byte[] jpg = this.compressYuvImage2Jpeg(data, params.getPreviewFormat(), Constants.COMPRESS_RATE << 1, size.width, size.height);
+//		byte[] scaledJpg = this.scaleJpeg(jpg, width, height, Constants.COMPRESS_RATE);
+//		return this.compressData(scaledJpg);
+		return this.compressData(jpg);
+	}
+	
+	private byte[] compressYuvImage2Jpeg(byte[] data, int format, int quality, int width, int height) {
+		YuvImage yuvImage = new YuvImage(data, format, width, height, null);
+		Rect rect = new Rect(0, 0, width, height);
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		yuvImage.compressToJpeg(rect, Constants.COMPRESS_RATE, out);
-		byte[] jpg = out.toByteArray();
+		yuvImage.compressToJpeg(rect, quality, out);
+		return out.toByteArray();
+	}
+	
+	private byte[] scaleNV21Image(byte[] data, int srcWidth, int srcHeight, int dstWidth, int dstHeight) {
+		int srcH = srcHeight >> 1;
+		int srcW = srcWidth >> 1;
+		int dstH = dstHeight >> 1;
+		int dstW = dstWidth >> 1;
 		
-		return compressData(jpg);
+		byte[] dst = new byte[dstW * dstH * 6];
+		int yRate = srcHeight / dstHeight;
+		int yFrac = srcH % dstH;
+		int xRate = srcWidth / dstWidth;
+		int xFrac = srcW % dstW;
+		int ye = 0;
+		int xe = 0;
+		
+		int srcOffset = srcWidth * srcHeight;
+		int dstOffset = ((dstW * dstH) << 2);
+		
+		int srcY = 0;
+		
+		for (int y = 0; y < dstH; y++) {
+			int dstBaseIndex4Y = dstW * 4 * y;
+			int srcBaseIndex4Y = srcW * 4 * srcY;
+			int dstBaseIndex4UV = dstW * 2 * y + dstOffset;
+			int srcBaseIndex4UV = srcWidth * srcY + srcOffset;
+			
+			int srcX = 0;
+			for (int x = 0; x < dstW; x++) {
+				// copy Y
+				int dstIndex4Y1 = dstBaseIndex4Y + (x << 1);
+				int dstIndex4Y2 = dstIndex4Y1 + (dstW << 1);
+				int srcIndex4Y1 = srcBaseIndex4Y + (srcX << 1);
+				int srcIndex4Y2 = srcIndex4Y1 + (srcW << 1);
+				System.arraycopy(data, srcIndex4Y1, dst, dstIndex4Y1, 2);
+				System.arraycopy(data, srcIndex4Y2, dst, dstIndex4Y2, 2);
+				
+				// copy UV
+				int dstIndex4UV = dstBaseIndex4UV + (x << 1);
+				int srcIndex4UV = srcBaseIndex4UV + (srcX << 1);
+				System.arraycopy(data, srcIndex4UV, dst, dstIndex4UV, 2);
+				
+				srcX += xRate;
+				xe += xFrac;
+				if (xe >= dstWidth) {
+					xe -= dstWidth;
+					srcX++;
+				}
+			}
+			
+			srcY += yRate;
+			ye += yFrac;
+			if (ye >= dstHeight) {
+				ye -= dstHeight;
+				srcY++;
+			}
+		}
+		
+		return dst;
+	}
+	
+	private byte[] scaleJpeg(byte[] jpg, int dstWidth, int dstHeight, int quality) {
+		Bitmap bmp = BitmapFactory.decodeByteArray(jpg, 0, jpg.length);
+		bmp = Bitmap.createScaledBitmap(bmp, dstWidth, dstHeight, false);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		bmp.compress(Bitmap.CompressFormat.JPEG, quality, out);
+		return out.toByteArray();
 	}
 	
 	private byte[] compressData(byte[] data) {
@@ -309,7 +385,7 @@ public class MainActivity extends Activity {
 		Collections.sort(sizeList, sizeComparator);
 		Camera.Size minSize = null;
 		if (!this.sizeList.isEmpty()) {
-			minSize = this.sizeList.get(0);
+			minSize = this.sizeList.get(this.sizeList.size() - 1);
 		}
 		if (minSize != null) {
 			params.setPreviewSize(Math.max(minSize.width, minSize.height), Math.min(minSize.width, minSize.height));
